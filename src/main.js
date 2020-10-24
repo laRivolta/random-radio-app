cf8b6935a6ef82121adcf986f326da2886dfab57
Fran Mosteiro <fran.mosteiro@holaluz.com>
2020-10-24 20:52:02 +0200

    Add lodash

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from "vue";
 import App from "@/ui/App.vue";
 import router from "@/ui/router";
 import store from "@/ui/store";
+
 import vuetify from "@/ui/plugins/vuetify";
+import VueLodash from "vue-lodash";
+import lodash from "lodash";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueLodash, { name: "custom", lodash: lodash });
 
 new Vue({
   router,
