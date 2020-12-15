cd5f541614978d393db184b378b14a1f1596bb02
Fran Mosteiro <fran.mosteiro@holaluz.com>
2020-12-15 20:47:29 +0100

    Custom radio component (styling). Also preventing to autoplay so src can change reactively

--- src/ui/components/RadioStation/Player.vue
+++ src/ui/components/RadioStation/Player.vue
@@ -1,8 +1,55 @@
 <template>
   <v-col :cols="8">
-    <audio id="radio" tabindex="0" controls class="hidden" preload="none">
-      <source :src="radioLink" />
-    </audio>
+    <div id="audio" class="player-wrapper">
+      <div class="player">
+        <div class="player-controls">
+          <div>
+            <a v-on:click.prevent="stop" title="Stop" href="#">
+              <svg
+                width="18px"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  fill="currentColor"
+                  d="M16,4.995v9.808C16,15.464,15.464,16,14.804,16H4.997C4.446,16,4,15.554,4,15.003V5.196C4,4.536,4.536,4,5.196,4h9.808C15.554,4,16,4.446,16,4.995z"
+                />
+              </svg>
+            </a>
+          </div>
+          <div>
+            <a
+              v-on:click.prevent="playing = !playing"
+              title="Play/Pause"
+              href="#"
+            >
+              <svg
+                width="18px"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  v-if="!playing"
+                  fill="currentColor"
+                  d="M15,10.001c0,0.299-0.305,0.514-0.305,0.514l-8.561,5.303C5.51,16.227,5,15.924,5,15.149V4.852c0-0.777,0.51-1.078,1.135-0.67l8.561,5.305C14.695,9.487,15,9.702,15,10.001z"
+                />
+                <path
+                  v-else
+                  fill="currentColor"
+                  d="M15,3h-2c-0.553,0-1,0.048-1,0.6v12.8c0,0.552,0.447,0.6,1,0.6h2c0.553,0,1-0.048,1-0.6V3.6C16,3.048,15.553,3,15,3z M7,3H5C4.447,3,4,3.048,4,3.6v12.8C4,16.952,4.447,17,5,17h2c0.553,0,1-0.048,1-0.6V3.6C8,3.048,7.553,3,7,3z"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+        <audio
+          ref="audiofile"
+          :src="radioLink"
+          preload="auto"
+          style="display: none;"
+        ></audio>
+      </div>
+    </div>
   </v-col>
 </template>
 
@@ -13,8 +60,21 @@ export default {
     link1: String,
     link2: String
   },
+  data: () => ({
+    audio: undefined,
+    loaded: false,
+    playing: false
+  }),
   mounted() {
-    this.playRadio();
+    this.audio = this.$el.querySelectorAll("audio")[0];
+    this.audio.addEventListener("timeupdate", this.update);
+    this.audio.addEventListener("loadeddata", this.load);
+    this.audio.addEventListener("pause", () => {
+      this.playing = false;
+    });
+    this.audio.addEventListener("play", () => {
+      this.playing = true;
+    });
   },
   computed: {
     radioLink: {
@@ -23,12 +83,121 @@ export default {
       }
     }
   },
+  watch: {
+    playing(value) {
+      if (value) {
+        return this.audio.play();
+      }
+      this.audio.pause();
+    }
+  },
   methods: {
     playRadio() {
       var player = document.getElementById("radio");
       player.play();
       player.focus();
+    },
+    load() {
+      if (this.audio.readyState >= 2) {
+        this.loaded = true;
+        this.durationSeconds = parseInt(this.audio.duration);
+        return (this.playing = this.autoPlay);
+      }
+
+      throw new Error("Failed to load sound file.");
+    },
+    seek(e) {
+      if (!this.playing || e.target.tagName === "SPAN") {
+        return;
+      }
+    },
+    stop() {
+      this.playing = false;
     }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css?family=Nunito:400,700");
+
+body {
+  font-family: "Nunito", sans-serif;
+}
+
+$player-bg: #fff;
+$player-border-color: darken($player-bg, 12%);
+$player-link-color: darken($player-bg, 75%);
+$player-progress-color: $player-border-color;
+$player-seeker-color: $player-link-color;
+$player-text-color: $player-link-color;
+
+.player-wrapper {
+  align-items: center;
+  background-color: $player-bg;
+  display: flex;
+  justify-content: center;
+}
+
+.player {
+  background-color: $player-bg;
+  border: 1px solid $player-border-color;
+  border-radius: 5px;
+  box-shadow: 0 5px 8px rgba(0, 0, 0, 0.15);
+  color: $player-text-color;
+  display: inline-block;
+  line-height: 1.5625;
+}
+
+.player-controls {
+  display: flex;
+
+  > div {
+    border-right: 1px solid $player-border-color;
+
+    &:last-child {
+      border-right: none;
+    }
+
+    a {
+      color: $player-link-color;
+      display: block;
+      line-height: 0;
+      padding: 1em;
+      text-decoration: none;
+    }
+  }
+}
+
+.player-progress {
+  background-color: $player-progress-color;
+  cursor: pointer;
+  height: 50%;
+  min-width: 200px;
+  position: relative;
+
+  .player-seeker {
+    background-color: $player-seeker-color;
+    bottom: 0;
+    left: 0;
+    position: absolute;
+    top: 0;
+  }
+}
+
+.player-time {
+  display: flex;
+  // font-size: 18px;
+  justify-content: space-between;
+
+  .player-time-current {
+    font-weight: 700;
+    padding-left: 5px;
+  }
+
+  .player-time-total {
+    opacity: 0.5;
+    padding-right: 5px;
+  }
+}
+</style>
